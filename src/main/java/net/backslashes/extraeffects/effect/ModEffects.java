d279dbbf29b9e88c99dac5ad991b097b15adcfcb
tt5 <tallonkh@gmail.com>
2026-07-11 15:01:50 -0700

    assets

--- src/main/java/net/backslashes/extraeffects/effect/ModEffects.java
+++ src/main/java/net/backslashes/extraeffects/effect/ModEffects.java
@@ -11,7 +11,7 @@ public class ModEffects {
     public static final net.neoforged.neoforge.registries.DeferredRegister<MobEffect> MOB_EFFECTS
             = DeferredRegister.create(Registries.MOB_EFFECT, ExtraEffects.MODID);
 
-    public static final DeferredHolder<MobEffect, Flight> FLIGHT = MOB_EFFECTS.register("flight_effect",
+    public static final DeferredHolder<MobEffect, Flight> FLIGHT = MOB_EFFECTS.register("flight",
             Flight::new);
 
     public static void register(IEventBus eventBus) {
